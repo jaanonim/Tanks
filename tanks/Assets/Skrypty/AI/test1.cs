79280e3ab787202a60d5d15685416730fc55bb29
jaanonim <mat8mro@gmail.com>
2018-03-12 10:27:40 +0100

    ok

---
+++ tanks/Assets/Skrypty/AI/test1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour {
+
+    public GameObject gora;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        gora.SendMessage("Hit",other);
+    }
+
+}
---
+++ tanks/Assets/Skrypty/AI/test2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test2 : MonoBehaviour {
+
+	public Transform target;
+    public GameObject G;
+    public Transform dol;
+    public int zycieEnemi = 50;
+    public Slider s;
+
+    void Update () {
+
+        transform.position = dol.position + new Vector3(0, 1f, 0);
+        s.value = zycieEnemi;
+        transform.LookAt(target);
+        float x=dol.eulerAngles.x;
+        float z = dol.eulerAngles.z;
+        float y = transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(x, y, z);
+        if (zycieEnemi<=0)
+        {
+            Destroy(G);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Hit(other);
+    }
+
+    public void Hit(Collider other)
+    {
+        if (other.tag == "pocisk")
+        {
+            zycieEnemi = zycieEnemi - 10;
+        }
+    }
+}
