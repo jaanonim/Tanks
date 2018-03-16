65e1a1254b095c494840d5fe4fd6c9d77903366b
jaanonim <mat8mro@gmail.com>
2018-03-16 19:34:22 +0100

    lol

---
+++ tanks/Assets/Settings.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    public AudioMixer mixer;
+
+    public Dropdown rd;
+
+    Resolution[] r;
+
+    public void Start()
+    {
+        r = Screen.resolutions;
+
+        rd.ClearOptions();
+
+        List<string> op = new List<string>();
+
+        int index = 0;
+        for(int i=0; i<r.Length; i++)
+        {
+            string ops = r[i].width + "x" + r[i].height;
+            op.Add(ops);
+
+            if (r[i].width == Screen.currentResolution.width && r[i].height == Screen.currentResolution.height)
+            {
+                index = i;
+            }
+        }
+
+        rd.AddOptions(op);
+        rd.value = index;
+        rd.RefreshShownValue();
+    }
+
+    public void Volume (float v)
+    {
+        mixer.SetFloat("v",v);
+	}
+
+    public void Qality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void full(bool f)
+    {
+        Screen.fullScreen = f;
+    }
+}
